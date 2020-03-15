4f1ebd57a654c727d07a85787819c3e0bbd761ff
Charlotte <charlotte.belot@mail.mcgill.ca>
2020-03-15 14:02:28 -0400

    new scripts organizing analysis

---
+++ R/04-graphics.R
@@ -0,0 +1,23 @@
+### 4 - MONTREAL ANALYSIS MAPS AND GRAPHS #############################################
+
+### Figure 2 - Map: Spatial distribution of listings at CMA extent ####################
+
+
+### Figure 3 - Map: Vacancy rates by census tract at CMA extent #######################
+
+
+### Figure 4 - Map: Spatial distribution of listings, 2016-2019 #######################
+
+
+### Figure 5 - Graph: Growth of active listings since 2015 ############################
+
+
+### Figure 6 - Graph: Listing growth by listing type since 2015 #######################
+
+
+### Figure 7 - Graph: Host revenue distribution in top percentiles ####################
+
+
+### Figure 8 - Graph: Growth of FREHs and FREH revenues since 2015 ####################
+
+
